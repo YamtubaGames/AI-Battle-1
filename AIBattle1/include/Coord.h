309607b542f34871eb69e405cd739391c1158166
duke11d <deneveandrew@gmail.com>
2017-10-12 14:54:48 -0500

    Added subtraction of Coord

--- AIBattle1/include/Coord.h
+++ AIBattle1/include/Coord.h
@@ -6,6 +6,17 @@ struct Coord
 {
     public:
         int x, y;
+        int operator-(Coord b) {
+            Coord c;
+            c.x = this->x - b.x;
+            if ((c.x) < 0)
+                c.x = -c.x;
+            c.y = this->y - b.y;
+            if ((c.y) < 0)
+                c.y = -c.y;
+            return c.x + c.y;
+        }
+
 };
 
 #endif // COORD_H
--- AIBattle1/src/UnitMovement.cpp
+++ AIBattle1/src/UnitMovement.cpp
@@ -1,9 +1,61 @@
 #include "UnitMovement.h"
 
-bool UnitMove(char Index, bool shooting, Coord From, Coord To, bool FromWalled, bool ToWalled)
+bool UnitMove(char Index, bool shooting, Coord From, Coord To, bool FromWalled = 0, bool ToWalled = 0)
 {
     switch(Index)
     {
+        case 0:
+            if(shooting)
+                return false;
+            else if(From - To > 2)
+                return false;
+            else
+            {
+
+            }
+            break;
+        case 1:
+            if(shooting)
+                return false;
+            else
+            {
+
+            }
+            break;
+        case 2:
+            if(shooting)
+            {
+
+            } else
+            {
+
+            }
+            break;
+        case 3:
+            if(shooting)
+                return false;
+            else
+            {
+
+            }
+            break;
+        case 4:
+            if(shooting)
+                return false;
+            else
+            {
+
+            }
+            break;
+        case 5:
+            if(shooting)
+            {
+
+            } else
+            {
+
+            }
+            break;
         default:
             return false;
     }
